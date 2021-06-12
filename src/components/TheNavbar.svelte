b4d2dca7a735904aa2686fa39f0bddbff7a57e53
bunykoder <>
2021-06-12 16:38:58 +0100

    Feature: Added characte insertion from navbar

--- src/components/TheNavbar.svelte
+++ src/components/TheNavbar.svelte
@@ -1,23 +1,93 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, onMount } from "svelte";
   const dispatch = createEventDispatcher();
+  export let wordLength;
+  export let charLength;
+  export let isDark;
   // Dispatches an event toggleDarkMode, which is listened to and used to toggle dark mode in app component
   let toggleDarkMode = () => {
     dispatch("toggleDarkMode");
   };
-  export let wordLength;
-  export let charLength;
-  export let isDark;
+  let insertMarkdownChar = (val, nextLine = false, shiftCursor = 0) => {
+    if (nextLine) {
+      document.querySelector("textarea").value = `${
+        document.querySelector("textarea").value + "\n" + val + " "
+      }`;
+    } else {
+      document.querySelector("textarea").value =
+        document.querySelector("textarea").value + val + " ";
+      //   if(shiftCursor){
+
+      //  FEATURE : simulate left arrow key press based on number passed as shiftCursor
+      //  }
+    }
+    document.querySelector("textarea").focus();
+  };
+  let insertHandler = (val) => {
+    switch (val.classList[0]) {
+      case "h1":
+        insertMarkdownChar("#", true);
+        break;
+      case "h2":
+        insertMarkdownChar("##", true);
+        break;
+      case "h3":
+        insertMarkdownChar("###", true);
+        break;
+      case "h4":
+        insertMarkdownChar("####", true);
+        break;
+      case "h5":
+        insertMarkdownChar("#####", true);
+        break;
+      case "h6":
+        insertMarkdownChar("######", true);
+        break;
+      case "tool-bold":
+        insertMarkdownChar("** **");
+        break;
+      case "tool-images":
+        insertMarkdownChar("![]()", true);
+        break;
+      case "tool-list-ul":
+        insertMarkdownChar(`-`, true);
+        break;
+      case "tool-list-ol":
+        insertMarkdownChar(`1.`, true);
+        break;
+      case "tool-links":
+        insertMarkdownChar("[]()");
+        break;
+      case "tool-del":
+        insertMarkdownChar("~~");
+        break;
+      case "tool-italics":
+        insertMarkdownChar("**");
+        break;
+      case "tool-quotes":
+        insertMarkdownChar(">", true);
+        break;
+      default:
+        break;
+    }
+  };
+  onMount(() => {
+    document.querySelectorAll(".nav-list li").forEach((el) => {
+      el.addEventListener("click", (e) => {
+        insertHandler(e.target);
+      });
+    });
+  });
 </script>
 
 <header
   class="pr-3 bg-red-50 dark:bg-gray-500 !max-w-full flex items-center justify-between"
 >
   <ul
-    class=" lg:flex w-[80%] hidden !max-w-full h-full items-center justify-around"
+    class=" lg:flex w-[80%] hidden !max-w-full h-full items-center justify-around nav-list"
   >
     <li
-      class="? h1 text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
+      class="h1 text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
     >
       h1
     </li>
@@ -49,17 +119,17 @@
     <li
       class="tool-bold text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
     >
-      <b>b</b>
+      <b class="tool-bold">b</b>
     </li>
     <li
       class="tool-italics text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
     >
-      <i>i</i>
+      <i class="tool-italics">i</i>
     </li>
     <li
-      class="tool-italics text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
+      class="tool-del text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
     >
-      <del>del</del>
+      <del class="tool-del">del</del>
     </li>
     <li
       class="tool-quotes text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
@@ -67,12 +137,12 @@
       “
     </li>
     <li
-      class="tool-quotes text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
+      class="tool-list-ul text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
     >
       •
     </li>
     <li
-      class="tool-quotes text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
+      class="tool-list-ol text-red-400 font-bold text-xl  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
     >
       1.
     </li>
@@ -87,9 +157,9 @@
       🖼️
     </li>
     <li
-      class="tool-save  py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
+      class="tool-save py-3 cursor-pointer hover:bg-red-100 border-r-2 border-red-200 dark:border-gray-400 dark:hover:bg-gray-400 dark:text-gray-900 border-dashed text-center px-4 transition-all duration-150 ease-linear "
     >
-      <button class="btn btn-green">export</button>
+      <button class="tool-save btn btn-green">export</button>
     </li>
   </ul>
   <div
